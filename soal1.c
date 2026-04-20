3110739904e7be99d93e0328bd0efc16def9c2c0
akidnana0 <aziizadityatama@gmail.com>
2026-04-20 09:23:29 +0700

    commit pertama

--- soal1.c
+++ soal1.c
@@ -1,5 +1,78 @@
 #include <stdio.h>
+#include <math.h>
 
-int main(void) {
+void BubbleSort(int JumlahBilangan, int Bilangan[]){
+   for (int i = 0; i < JumlahBilangan-1; i++){
+       for (int j = 0; j < JumlahBilangan-i-1; j++){
+           if (Bilangan[j] > Bilangan[j+1]){
+               int temp = Bilangan[j];
+               Bilangan[j] = Bilangan[j+1];
+               Bilangan[j+1] = temp;
+           }
+       }
+   }
+}
+
+
+int main(){
+    int N = 0;
+    int MAX = 0;
+    scanf("%d", &N);
+    int Array[N];
+
+
+    for (int i = 0; i < N; i++){
+        scanf("%d", &Array[i]);
+    }
+    for (int j = 0; j < N; j++){
+        // int value_left;
+        // int has_left = 1;
+        // int value_right;
+        // int has_right = 1;
+        if (Array[j] == -1){
+            if ((Array[j-1] != -1) && (Array[j+1] != -1)){
+                Array[j] = floor(((double)Array[j-1] + (double)Array[j+1])/2);
+            }
+            else if ((Array[j-1] != -1) && (Array[j+1] == -1)){
+                for (int k = 0; k < N; k++){
+                    if (Array[j+k+1] != -1){
+                        Array[j] = floor(((double)Array[j-1] + (double)Array[j+k+1])/2);
+                        break;
+                    }
+                }
+            }
+            else if((Array[j-1] == -1) && (Array[j+1] != -1)){
+                for (int k = 0; k < N; k++){
+                    if (Array[j-k-1] != -1){
+                        Array[j] = floor(((double)Array[j-k-1] + (double)Array[j+1])/2);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+    printf("RECOVERED ");
+    for (int l = 0; l < N; l++){
+        printf("%d ", Array[l]);
+    }
+
+    printf("\n");
+
+    BubbleSort(N, Array);
+    for (int p = 0; p < N; p++){
+        if(Array[N-1]== -2){
+            MAX = -2;
+        }
+        else if (Array[p] < -1){
+            continue;
+        }
+        else {
+            MAX = MAX + Array[p];
+        }
+    }
+
+    printf("MAX_SUM %d", MAX);
+
     return 0;
 }
